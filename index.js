2eecc8e81c0f18cbea12b76f582136338788e7c8
Joaquín Windmuller <joaquin@windmuller.ca>
2020-11-02 09:48:53 -0700

    Started work on handling new UI changes
    Episode name is not available anymore so to group the episodes it uses the art URL.
    
    Now shows a loading animation for the title and it loads the first episode and grabs the title from there.
    
    Switched to using string as a template for the html instead of creating the elements in code

--- index.js
+++ index.js
@@ -1,70 +1,105 @@
-const episodes = Array.from(document.getElementsByClassName('episodecell'));
-const container = episodes[0].previousElementSibling;
-let podcasts = {};
+(async () => {
+    const episodes = Array.from(document.getElementsByClassName('episodecell'));
+    const container = episodes[0].previousElementSibling;
+    let podcasts = {};
+    var episodeRequests = episodes.map(episodeCell => {
+        const artURL = episodeCell.getElementsByClassName('art')[0].src;
+        let promise = null;
+        if (!podcasts[artURL]) {
 
-episodes.forEach(episodeCell => {
-    const podcastName = episodeCell.getElementsByClassName('caption2')[0].innerText.trim();
-    if (!podcasts[podcastName]) {
-        podcasts[podcastName] = {
-            name: podcastName,
-            image: episodeCell.getElementsByClassName('art')[0].cloneNode(),
-            episodes: []
+            podcasts[artURL] = {
+                artURL,
+                image: episodeCell.getElementsByClassName('art')[0].cloneNode(),
+                episodes: [],
+                name: 'Podcast name not found'
+            }
+            promise = new Promise((resolve, reject) => {
+                fetch(episodeCell.href).then(async episodeResponse => {
+                    var episodeResponseBodyReader = episodeResponse.body.getReader();
+                    let title = 'Title not found';
+                    try {
+                        let { value: chunk, done: readerDone } = await episodeResponseBodyReader.read();
+                        const utf8Decoder = new TextDecoder('utf-8');
+                        chunk = chunk ? utf8Decoder.decode(chunk) : '';
+                        const div = document.createElement('div');
+                        div.innerHTML = chunk;
+                        let h3s = div.getElementsByTagName('h3');
+
+                        if (h3s.length === 1) {
+                            podcasts[artURL].name = h3s[0].innerText.trim();
+                        }
+                    } catch (e) { }
+                    resolve('success');
+                }).catch(async e => {
+                    resolve('error');
+                });
+            });
         }
-    }
-    podcasts[podcastName].episodes.push(episodeCell);
-});
-
-const wrappers = Object.keys(podcasts).sort().map(podcastName => {
-        return renderEpisodeContainer(podcasts[podcastName]);
-});
-
-wrappers.forEach(wrapper => {
-    container.insertAdjacentElement('afterend', wrapper);
-})
-
-
-function renderEpisodeContainer(podcast) {
-    const wrapper = document.createElement('div');
-    const episodesContainer = document.createElement('div');
-    const header = document.createElement('a');
-    const title = document.createElement('h3');
-    const episodeCount = podcast.episodes.length;
-    title.classList.add('boc-title');
-    title.innerText = podcast.name;
-    title.setAttribute('data-episode-count',
-        `${episodeCount} ${episodeCount === 1 ? 'episode' : 'episodes'}`
-    );
-
-    header.setAttribute('href', '#');
-    header.classList.add('boc-podcast-title-container');
-    header.appendChild(podcast.image);
-    header.appendChild(title);
-    
-    wrapper.appendChild(header);
-    episodesContainer.classList.add('boc-episode-container');
-    episodesContainer.classList.add('boc-collapsed-episode-container');
-
-    podcast.episodes.forEach(episodeCell => {
-        episodesContainer.appendChild(episodeCell);
+        podcasts[artURL].episodes.push(episodeCell);
+        return promise;
     });
 
-    wrapper.appendChild(episodesContainer);
+    const wrappers = Object.keys(podcasts).sort().map(artURL => {
+        podcasts[artURL].container = renderEpisodeContainer(podcasts[artURL]);
+        return podcasts[artURL].container;
+    });
 
-    header.addEventListener('click', toggleOpen);
+    wrappers.forEach(wrapper => {
+        container.insertAdjacentElement('afterend', wrapper);
+    })
 
-    return wrapper;
-}
 
-function toggleOpen(event) {
-    event.preventDefault();
-    let clickedTitle = event.target;
-    if (clickedTitle.nodeName != 'A') {
-        clickedTitle = clickedTitle.parentElement;
+    function renderEpisodeContainer(podcast) {
+        const tpl = `
+<div class="boc-podcast-container">
+    <div class="boc-podcast-art-container">
+        <img class="boc-podcast-art" src="${podcast.artURL}" />
+    </div>
+    <div class="boc-episodes-container">
+        <h3 class="boc-podcast-title loading">...</h3>
+    </div>
+</div>
+    `;
+
+        const container = document.createElement('div');
+        container.innerHTML = tpl;
+        const episodesContainer = container.getElementsByClassName('boc-episodes-container')[0];
+
+        podcast.episodes.forEach(episodeCell => {
+            episodesContainer.appendChild(episodeCell);
+        });
+
+        container.addEventListener('click', toggleOpen);
+
+        return container;
     }
-    const container = clickedTitle.nextElementSibling;
-    if (container.classList.contains('boc-collapsed-episode-container')) {
-        container.classList.remove('boc-collapsed-episode-container');
-    } else {
-        container.classList.add('boc-collapsed-episode-container');
+
+    function toggleOpen(event) {
+        event.preventDefault();
+        debugger
+        let clickedTitle = event.target;
+        if (clickedTitle.nodeName != 'A') {
+            clickedTitle = clickedTitle.parentElement;
+        }
+        const container = clickedTitle.nextElementSibling;
+        if (container.classList.contains('boc-collapsed-episode-container')) {
+            container.classList.remove('boc-collapsed-episode-container');
+        } else {
+            container.classList.add('boc-collapsed-episode-container');
+        }
     }
-}
+
+
+    console.log('waiting now...', podcasts);
+    await Promise.all(episodeRequests);
+    Object.keys(podcasts).forEach(artURL => {
+        var podcastInfo = podcasts[artURL];
+        if (!podcastInfo.container) {
+            return;
+        }
+        var titleContainer = podcastInfo.container.getElementsByClassName('boc-podcast-title')[0];
+        titleContainer.innerText = podcastInfo.name;
+        titleContainer.classList.remove('loading');
+    });
+
+})();
