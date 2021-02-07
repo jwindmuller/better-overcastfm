4cc4f201d42cdf3ac0724a1c4f1964809d5bb7ea
Joaquin Windmuller <joaquin@windmuller.ca>
2021-02-06 19:44:00 -0700

    Refactor build script into separate library

--- build/index.js
+++ build/index.js
@@ -1,48 +1,22 @@
-const fs = require('fs');
+
 const { argv } = require('process');
-const archive = require('node-zip')();
-const semver = require('semver');
-const simpleGit = require('simple-git');
-const files = [
-    'icon16.png',
-    'icon48.png',
-    'icon128.png',
-    'icon1024.png',
-    'index.js',
-    'styles.css',
-    'manifest.json'
-];
+const lib = require('./lib');
+let [, , incrementType, doCommits] = argv;
 
-let manifest = JSON.parse(fs.readFileSync('manifest.json'));
-let version = semver.parse(manifest.version);
-version = version.inc(argv[2] || 'patch').format();
-manifest.version = version;
-fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 4) + '\n');
+if (doCommits === undefined) {
+    doCommits = true;
+}
+if (incrementType === 'dry') {
+    incrementType = 'patch';
+    doCommits = false;
+}
 
-let package = JSON.parse(fs.readFileSync('package.json'));
-package.version = version;
-fs.writeFileSync('package.json', JSON.stringify(package, null, 4));
+console.log({incrementType, doCommits});
 
-files.forEach(fileName => {
-    archive.file(fileName, fs.readFileSync(fileName) + '\n');
-});
+const version = lib.updateVersionInFiles(incrementType);
 
-const zipFilePath = '../better-overcast.zip';
-fs.writeFileSync(
-    zipFilePath,
-    archive.generate({ base64: false, compression: 'DEFLATE' }),
-    'binary'
-);
-console.log(`Generated ${process.cwd()}${zipFilePath}`);
-
-const doGit = async (version) => {
-    const git = simpleGit();
-
-    version = `v${version}`
-    await git.add('.');
-    await git.commit(version);
-    await git.addTag(version);
-    await git.push();
-    await git.pushTags();
-};
-doGit(version);
+if (doCommits) {
+    lib.doGit(version);
+} else {
+    console.log('Skipped git actions');
+}
---
+++ build/lib.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const archive = require('node-zip')();
+const semver = require('semver');
+const simpleGit = require('simple-git');
+const path = require('path');
+
+
+const updateVersionInFiles = (incrementType) => {
+    incrementType = incrementType || 'patch';
+    
+    const files = [
+        'icon16.png',
+        'icon48.png',
+        'icon128.png',
+        'icon1024.png',
+        'index.js',
+        'styles.css',
+        'manifest.json'
+    ];
+    let manifest = JSON.parse(fs.readFileSync('manifest.json'));
+    let version = semver.parse(manifest.version);
+    version = version.inc(incrementType).format();
+    manifest.version = version;
+    fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 4) + '\n');
+    
+    let package = JSON.parse(fs.readFileSync('package.json'));
+    package.version = version;
+    fs.writeFileSync('package.json', JSON.stringify(package, null, 4));
+    
+    files.forEach(fileName => {
+        archive.file(fileName, fs.readFileSync(fileName) + '\n');
+    });
+    
+    const zipFilePath = 'better-overcast.zip';
+    fs.writeFileSync(
+        `../${zipFilePath}`,
+        archive.generate({ base64: false, compression: 'DEFLATE' }),
+        'binary'
+    );
+    console.log(`Generated ${zipFilePath} in ${path.resolve(process.cwd(), '..')}`);
+
+    return version;
+};
+
+const doGit = async (version) => {
+    const git = simpleGit();
+
+    version = `v${version}`
+    await git.add('.');
+    await git.commit(version);
+    await git.addTag(version);
+    await git.push();
+    await git.pushTags();
+};
+
+module.exports = {
+    updateVersionInFiles,
+    doGit
+};
